475015a1c3a995ce9a52d9b8a549259b36a3e38a
ab1228 <alexb8734@gmail.com>
2019-11-01 23:41:27 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// Book class: represetnts book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.auhtor = author;
+        this.isbn = isbn;
+    }
+}
+// UI class: Handle UI tasks
+
+// Store class: Handle stroage 
+
+//Event: Display books
+
+//Event: Add a Book
+
+//Event: Remove a Book 
