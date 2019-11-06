bef2ed6e90188aecd6823a5206a13f4570517625
ab1228 <alexb8734@gmail.com>
2019-11-06 12:46:38 -0600

    changed to query selector

--- app.js
+++ app.js
@@ -11,35 +11,51 @@ class UI {
     static displayBooks() {
         var storedBooks = [
             {
-                tiitle: 'Book One',
+                title: 'Book One',
                 author: 'John Doe',
                 isbn: '3345004934'
             },
             {
-                tiitle: 'Book Two',
+                title: 'Book Two',
                 author: 'Mike Will',
                 isbn: '3345004934'
             }
         ];
-        var books = storedBooks;
-
-        books.forEach(function (book) {
-            UI.addBookToList(book);
-        })
+        const books = storedBooks;
+        // console.log(books);
+        // books.forEach(function (book) {
+        //     UI.addBookToList(book);
+        books.forEach((book) => UI.addBookToList(book));
+        // })
+        console.log(book);
     };
 
-    static addBookToList(book) {
-        var list = document.getElementById('#book-list');
-        var row = document.createElement('tr');
 
-        row.innerHTML = '<td>'
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
+
+        row.innerHTML =
+            `<td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href= "#" class="btn btn-danger btn-sm delete ">X</a></td>`
+
+        list.appendChild(row);
+
     }
+
 
 }
 // Store class: Handle stroage
 
 //Event: Display books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add a Book
-
-//Event: Remove a Book 
+document.querySelector("#book-form").addEventListener("submit", function () {
+    const titile = document.querySelector("#titile").value;
+    const titile = document.querySelector("#author").value;
+    const titile = document.querySelector("#isbn").value;
+})
+//Event: Remove a Book
